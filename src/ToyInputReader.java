a0f77704281bca554df5b129e66111ea0bbcee00
erwinf92 <erwinfernandez92@gmail.com>
2017-10-05 17:42:35 +1100

    Creating input reader class
    added a class that has the main funtion as the input reader.

---
+++ src/ToyInputReader.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ToyInputReader {
+	
+	private ArrayList<String> toyCommandList = new ArrayList<String>();
+	private String fileName;
+	
+	public ToyInputReader (String fn) {
+		fileName =fn;
+	}
+	
+	public ToyInputReader() {
+		//default filename for input is input.txt if not given
+		fileName = "input.txt";
+	}
+	
+	public void readInput() {
+		String line;
+		
+		try {
+			FileReader fileReader = new FileReader(fileName);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			
+			while ( (line = bufferedReader.readLine())!= null) {
+				toyCommandList.add(line);
+			}
+			
+			bufferedReader.close();
+		}
+		catch(FileNotFoundException ex) {
+			System.out.println("Unable to open file "+fileName);
+		}
+		catch(IOException ex) {
+			System.out.println("Error reading file "+ fileName);
+		}
+	}
+
+	public ArrayList<String> getToyCommandList() {
+		return toyCommandList;
+	}
+	
+}
