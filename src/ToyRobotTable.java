e58052a970f19d37caba6f1d307ab0945f4574df
erwinf92 <erwinfernandez92@gmail.com>
2017-10-10 13:30:43 +1100

    adding attribute and generator for ToyRobotTable
    includng the getter and setter for all attributes.

--- src/ToyRobotTable.java
+++ src/ToyRobotTable.java
@@ -1,4 +1,54 @@
 
 public class ToyRobotTable {
+	private int bottomLeftX;
+	private int bottomLeftY;
+	private int tableLength;
+	private int tableWidth;
 
+	public int getBottomLeftX() {
+		return bottomLeftX;
+	}
+
+	public void setBottomLeftX(int bottomLeftX) {
+		this.bottomLeftX = bottomLeftX;
+	}
+
+	public int getBottomLeftY() {
+		return bottomLeftY;
+	}
+
+	public void setBottomLeftY(int bottomLeftY) {
+		this.bottomLeftY = bottomLeftY;
+	}
+
+	public int getTableLength() {
+		return tableLength;
+	}
+
+	public void setTableLength(int tableLength) {
+		this.tableLength = tableLength;
+	}
+
+	public int getTableWidth() {
+		return tableWidth;
+	}
+
+	public void setTableWidth(int tableWidth) {
+		this.tableWidth = tableWidth;
+	}
+
+	//assuming bottom left of the table is at 0,0 when not given
+	public ToyRobotTable (int length, int width) {
+		bottomLeftX = 0;
+		bottomLeftY = 0;
+		tableLength = length;
+		tableWidth = width;
+	}
+	
+	public ToyRobotTable (int x, int y, int length, int width) {
+		bottomLeftX = x;
+		bottomLeftY = y;
+		tableLength = length;
+		tableWidth = width;
+	}
 }
